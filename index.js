a6364dd6ece33047382066e5f4b89885776ad183
Tekton Pro Admin <info@tektonpro.dev>
2024-03-06 19:22:00 -0500

    adds initial pug markup

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-const express = require('express');
-const axios = require('axios');
+const express = require("express");
+const axios = require("axios");
 const app = express();
 
-app.set('view engine', 'pug');
-app.use(express.static(__dirname + '/public'));
+app.set("view engine", "pug");
+app.use(express.static(__dirname + "/public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = "";
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
-
+// ✅ Create a new pug template called updates in the views folder.
+// 👉🏾 Render the updates template and pass along a page title called Update Custom Object Form | Integrating With HubSpot I Practicum.
+// In the updates pug template, add a link “Return to the homepage” that links to the root route.
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
@@ -66,6 +68,5 @@ app.post('/update', async (req, res) => {
 });
 */
 
-
 // * Localhost
-app.listen(3000, () => console.log('Listening on http://localhost:3000'));
+app.listen(3000, () => console.log("Listening on http://localhost:3000"));
